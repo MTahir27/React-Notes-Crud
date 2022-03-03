d852f18c0aba42ee4134fc2ad6481bc51316e696
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-03 21:49:40 +0500

    Input Field Component Created

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import CardComponent from "./Components/Card";
+import InputField from "./Components/InputField";
 
 function App() {
   return (
     <>
       <Header />
       <CardComponent />
+      <InputField />
     </>
   );
 }
