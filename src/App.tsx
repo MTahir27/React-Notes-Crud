a2a2b75bf8048322907c52532b21c0501bb75e9e
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-03 22:09:50 +0500

    Abc

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import CardComponent from "./Components/Card";
-import InputField from "./Components/InputField";
 
 function App() {
   return (
     <>
       <Header />
       <CardComponent />
-      <InputField />
     </>
   );
 }
