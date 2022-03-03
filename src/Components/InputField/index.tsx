0ea52eca89bd3057421c6bf815fda5a4d9c37d8f
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-03 23:21:25 +0500

    Id Pass

--- src/Components/InputField/index.tsx
+++ src/Components/InputField/index.tsx
@@ -1,15 +1,16 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-interface InputField{
-  label: string,
-  placeholder?: string,
-  multiline?: any,
-  rows?: number,
+interface InputField {
+  label: string;
+  placeholder?: string;
+  id?: string;
+  multiline?: any;
+  rows?: number;
 }
 
-export default function InputField({label, ...otherInputProps}:InputField) {
+export default function InputField({ label, ...otherInputProps }: InputField) {
   return (
     <Box
       sx={{
@@ -23,7 +24,7 @@ export default function InputField({label, ...otherInputProps}:InputField) {
         id="demo-helper-text-aligned-no-helper"
         label={label}
         {...otherInputProps}
-        style={{textTransform: "capitalize"}}
+        style={{ textTransform: "capitalize" }}
       />
     </Box>
   );
