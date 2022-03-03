34b7021082dbe1e0e9f17fd8e8a676c957eb1a53
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-03 20:52:56 +0500

    Header Component Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Header from "./Components/Header";
 import CardComponent from "./Components/Card";
 
 function App() {
   return (
-    <CardComponent />
+    <>
+      <Header />
+      <CardComponent />
+    </>
   );
 }
 
