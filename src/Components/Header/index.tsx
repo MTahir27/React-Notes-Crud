a8ce094f83d90ed3751b81ebe2b701f5bb987a85
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-03 21:45:15 +0500

    Header Styling

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,6 @@
-import React, { FC } from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Logo from "../../Assets/Images/Logo/logo.png";
 
 export default function Header() {
