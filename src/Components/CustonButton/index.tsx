ba778253315c1c4e3cb3010872d7c4839aa65080
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-04 17:58:34 +0500

    Button Error Removed

--- src/Components/CustonButton/index.tsx
+++ src/Components/CustonButton/index.tsx
@@ -1,12 +1,21 @@
 import Button from "@mui/material/Button";
 interface ButtonType {
   text: string;
-  size?: string;
-  color?: string;
-  variant?: string;
+  size?: "small" | "medium" | "large" | undefined;
+  color?:
+    | "inherit"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning"
+    | undefined;
+  variant?: "contained" | "text" | "outlined" | undefined;
   startIcon?: any;
   endIcon?: any;
 }
+
 export default function CustomButton({
   text,
   ...otherButtonProps
